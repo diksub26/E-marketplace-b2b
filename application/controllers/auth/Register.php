c5f742df5d730bd7142849e04b5d9652871cc3a0
diksub <diki@dev.netkromsolution.com>
2020-03-09 16:57:49 +0700

    init register

---
+++ application/controllers/auth/Register.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * 
+ * @package App
+ * @category Controller
+ * @author diksub
+ */
+
+ class Register extends MY_Controller
+ {
+     
+ }
+ 
