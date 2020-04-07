da7206fce966457720f1c7e23bacdb6bde296cf8
diksub <diki@dev.netkromsolution.com>
2020-04-07 20:45:03 +0700

    Fixing bug on save data rules menu

--- application/views/managament_menu/rules/index.php
+++ application/views/managament_menu/rules/index.php
@@ -240,7 +240,7 @@
             $('#'+data.roles_name+' > .x_panel >.x_content').html('');
             $('#'+data.roles_name+' > .x_panel >.x_content').html(data.html);
             if ($(".js-switch")[0]) {
-              var elems = Array.prototype.slice.call(document.querySelectorAll('.js-switch'));
+              var elems = Array.prototype.slice.call(document.querySelectorAll('#'+data.roles_name+' > .x_panel >.x_content .js-switch'));
               elems.forEach(function (html) {
                 var switchery = new Switchery(html, {
                   color: '#26B99A',
